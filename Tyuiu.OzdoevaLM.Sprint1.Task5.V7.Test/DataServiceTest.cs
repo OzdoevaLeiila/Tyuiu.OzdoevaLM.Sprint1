12c411d1680c6a55208d8e03b7927747e8213c32
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-02 21:12:38 +0500

    Task5

---
+++ Tyuiu.OzdoevaLM.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task5.V7.Lib;
+
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double f = 90;
+            int res = ds.AngleToHoursMinutes(f);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 3;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
