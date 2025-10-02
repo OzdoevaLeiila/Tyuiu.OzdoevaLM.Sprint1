1485a6d3065ffa3c0ae4856799830bf55c61f95b
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-02 19:04:42 +0500

    Task4

---
+++ Tyuiu.OzdoevaLM.Sprint1.Task4.V13/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task4.V13.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task4.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Оздоева Л. М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Оздоева Лейла Магомедовна | ИИПб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("(cos(П/x)) / (3 * e^(x + y)) = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+
+        }
+    }
+}
