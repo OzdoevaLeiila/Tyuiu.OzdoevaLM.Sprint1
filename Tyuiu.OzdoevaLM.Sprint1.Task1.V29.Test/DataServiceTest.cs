507c1eae345c71ba35c86c6925970ae71b5ece2f
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-02 12:30:45 +0500

    Task1

---
+++ Tyuiu.OzdoevaLM.Sprint1.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task1.V29.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task1.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 1.0;
+            double b = 2.0;
+            double c = 3.0;
+            var res = ds.Calculate(a, b, c);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
