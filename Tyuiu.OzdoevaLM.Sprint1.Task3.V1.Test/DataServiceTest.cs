e4e42d4d1bb923e2aa0295b8a30956c7c94ab215
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-02 17:04:31 +0500

    Task3

---
+++ Tyuiu.OzdoevaLM.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task3.V1.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double r = 3;
+            double h = 5;
+            double wait = 141.372;
+            var res = ds.CylinderVolume(r, h);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint1.Task3.V1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task3.V1.Lib;
+
+//Объявите необходимые переменные и напишите программу вычисления объема цилиндра, предполагающий ввод исходных данных.
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Оздоева Л. М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Оздоева Лейла Магомедовна | ИИПб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите радиус цилиндра:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите высоту цилиндра:");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Радиус цилиндра = " + r);
+            Console.WriteLine("Высота цилиндра = " + h);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Объём цилиндра = " + ds.CylinderVolume(r, h));
+
+            Console.ReadKey();
+        }
+    }
+}
