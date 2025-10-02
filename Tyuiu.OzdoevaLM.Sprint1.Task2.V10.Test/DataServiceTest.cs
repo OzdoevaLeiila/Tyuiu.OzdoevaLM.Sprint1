284a53621ce67cdf2b537edd3abb22ba789b53df
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-02 15:22:49 +0500

    Task2

---
+++ Tyuiu.OzdoevaLM.Sprint1.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task2.V10.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task2.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int metr = 2;
+            var res = ds.ConvertMetreToInchs(metr);
+            Assert.AreEqual(78.740, res);
+        }
+    }
+}
