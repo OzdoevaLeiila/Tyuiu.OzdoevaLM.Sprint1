f3a051ac1e58f24b3e11173e4df58014f0d1d3a6
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-03 00:31:49 +0500

    Task7

--- Tyuiu.OzdoevaLM.Sprint1.Task7.V26.Test/DataServiceTest.cs
+++ Tyuiu.OzdoevaLM.Sprint1.Task7.V26.Test/DataServiceTest.cs
@@ -13,7 +13,6 @@ namespace Tyuiu.OzdoevaLM.Sprint1.Task7.V26.Test
             double x = 2;
             double y = 4;
             double wait = 1.071;
-
             double res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
         }
