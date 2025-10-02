992eb37cc70bec80dc41f686cf16743602b9b33f
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-10-02 22:53:33 +0500

    Task6

---
+++ Tyuiu.OzdoevaLM.Sprint1.Task6.V9.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task6.V9.Lib
+{
+    public class DataService : ISprint1Task6V9
+    {
+        public string MoveLetterToStart(string value)
+        {
+            string result = "";
+            foreach (string word in value.Split())
+
+                result += word[word.Length - 1] + word.Remove(word.Length - 1) + " ";
+            return result.Trim();
+        }
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint1.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.OzdoevaLM.Sprint1.Task6.V9.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint1.Task6.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+
+            string strTest = "hello world program";
+            string res = ds.MoveLetterToStart(strTest);
+            string wait = "ohell dworl mprogra";
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
